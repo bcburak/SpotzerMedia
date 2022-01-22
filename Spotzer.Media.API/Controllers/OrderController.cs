f0e4c14b5ac65656e1c46813d87c7a493a0122fd
Burak <burak.celik@kron.com.tr>
2022-01-22 19:58:30 +0300

    initial prototype with swagger support

---
+++ Spotzer.Media.API/Controllers/OrderController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spotzer.Media.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+        // GET: HomeController
+        private readonly ILogger<OrderController> _logger;
+
+        public OrderController(ILogger<OrderController> logger)
+        {
+            _logger = logger;
+        }
+
+      
+        [HttpPost("AddOrder")]
+        public IActionResult AddOrder([FromBody] dynamic order)
+        {
+            //try
+            //{
+            //    _logger.LogInformation("Test");
+            //    return CreatedAtAction("User Created : ", new { user.FirstName }, user);
+            //}
+            //catch (Exception)
+            //{
+            //    return new JsonResult("Something wrong") { StatusCode = 500 };
+            //    throw;
+            //}
+            return Ok();
+        }
+
+    }
+}
---
+++ Spotzer.Media.API/Startup.cs
@@ -0,0 +1,88 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+
+namespace Spotzer.Media.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddMvcCore().AddApiExplorer();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Spotzer.Media.API", Version = "v1" });
+            });
+            
+        }
+
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();               
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Spotzer.Media.API v1"));
+            app.UseRouting();
+
+
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    var logger = appError.ApplicationServices.GetRequiredService<ILogger<Startup>>();
+                    var exception = context.Features.Get<IExceptionHandlerFeature>();
+
+                    if(exception.Error != null)
+                    {
+                        logger.LogError(exception.Error, "Exception Here");
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        context.Response.ContentType = "application/json";
+                        await context.Response.WriteAsync(System.Text.Json.JsonSerializer.Serialize(new
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = "Internal Server Error. "
+                        }));
+                    }                  
+
+                });
+            });
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+ 
+
+        }
+    }
+}
